cc74595906f7cfcaa7de39c6dbe12bacc8e53b80
Mathias Bauer <mba@mba.codes>
2015-09-02 15:21:43 +0200

    merge 'context' and 'extra' if 'extra' exists #monolog-mysql-1

--- src/MySQLHandler/MySQLHandler.php
+++ src/MySQLHandler/MySQLHandler.php
@@ -120,6 +120,15 @@ class MySQLHandler extends AbstractProcessingHandler {
             $this->initialize();
         }
 
+        /*
+         * merge $record['context'] and $record['extra'] as additional info of Processors
+         * getting added to $record['extra']
+         * @see https://github.com/Seldaek/monolog/blob/master/doc/02-handlers-formatters-processors.md
+         */
+        if (isset($record['extra'])) {
+            $record['context'] = array_merge($record['context'], $record['extra']);
+        }
+
         //'context' contains the array
         $contentArray = array_merge(array(
             'channel' => $record['channel'],
