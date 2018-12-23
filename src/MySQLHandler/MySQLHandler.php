fbaf811538a97ff128a663c1e79a3269a38baa74
Pchol <spchols@gmail.com>
2018-12-23 12:35:19 +0300

    strict compare type in array, avoid bug in_array 0 value

--- src/MySQLHandler/MySQLHandler.php
+++ src/MySQLHandler/MySQLHandler.php
@@ -188,7 +188,7 @@ class MySQLHandler extends AbstractProcessingHandler
 
         // unset array keys that are passed put not defined to be stored, to prevent sql errors
         foreach($contentArray as $key => $context) {
-            if (! in_array($key, $this->fields)) {
+            if (! in_array($key, $this->fields, true)) {
                 unset($contentArray[$key]);
                 unset($this->fields[array_search($key, $this->fields)]);
                 continue;
