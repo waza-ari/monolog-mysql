0b1fa6d0f51b2ff799be8d880847a8edb7fcf144
czachor <f@czachorowski.pro>
2020-04-06 12:44:35 +0200

    fix: if not all values of additional fields are provided, then an exception is thrown: "SQLSTATE[HY093]: Invalid parameter number: number of bound variables does not match number of tokens"

--- src/MySQLHandler/MySQLHandler.php
+++ src/MySQLHandler/MySQLHandler.php
@@ -206,8 +206,8 @@ class MySQLHandler extends AbstractProcessingHandler
         $this->prepareStatement();
 
 	    //Remove unused keys
-	    foreach($this->additionalFields as $key => $context) {
-		    if(! isset($contentArray[$key])) {
+	    foreach ($this->additionalFields as $key => $context) {
+		    if (isset($contentArray[$key])) {
 			    unset($this->additionalFields[$key]);
 		    }
 	    }
